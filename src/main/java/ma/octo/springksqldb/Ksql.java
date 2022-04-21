89832e123f1ba9a98647077930aed6ef681e2316
AbidarYassine <yassine.abidar@edu.uca.ma>
2022-04-21 20:50:32 +0000

    update

--- src/main/java/ma/octo/springksqldb/Ksql.java
+++ src/main/java/ma/octo/springksqldb/Ksql.java
@@ -9,20 +9,11 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class Ksql {
-//    private static KsqlDBApplication ksqlDBApplication;
-
     public static void main(String[] args) {
         ClientOptions options = ClientOptions.create()
                 .setHost("localhost")
                 .setPort(8088);
         Client client = Client.create(options);
-//        System.out.println("KSQL client created " + client);
-//        ksqlDBApplication = new KsqlDBApplication(client);
-//        createAlertsMaterializedView();
-//        RowSubscriber<Alert> alertSubscriber = new RowSubscriber<>();
-
-//        CompletableFuture<Void> result = ksqlDBApplication.subscribeOnAlerts(alertSubscriber);
-//        System.out.println("result = " + result);
         String pullQuery = "SELECT CUSTOMER_ID, FIRST_NAME, LAST_NAME, EMAIL, COMMENTS FROM USERS EMIT CHANGES LIMIT 5;";
         // Terminate any open connections and close the client
         BatchedQueryResult batchedQueryResult = client.executeQuery(pullQuery);
@@ -40,9 +31,4 @@ public class Ksql {
         }
 
     }
-
-//    private static void createAlertsMaterializedView() {
-//        ksqlDBApplication.createReadingsStream().join();
-//        ksqlDBApplication.createAlertsTable().join();
-//    }
 }
